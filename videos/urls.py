4faa0e7486759ace7a5e9a6b8d2ab911a647e689
Charles Montemayor <montemayorcharles@outlook.com>
2020-07-07 22:05:14 -0600

    video delete creation

--- videos/urls.py
+++ videos/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from .views import VideoListView, VideoDetailView
+from .views import (
+    VideoListView,
+    VideoDetailView,
+    VideoCreateView,
+    VideoDeleteView,
+    )
 
 urlpatterns = [
     path('', VideoListView.as_view(), name='video_list'),
+    path('<uuid:pk>/delete/', VideoDeleteView.as_view(), name='video_delete'),
+    path('new/', VideoCreateView.as_view(), name='video_new'),
     path('<slug:slug>', VideoDetailView.as_view(), name='video_detail'),
+    
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import PermissionDenied
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import Video
 
@@ -12,3 +15,23 @@ class VideoDetailView(DetailView):
     model = Video
     template_name = 'videos/video_detail.html'
     context_object_name = 'video'
+
+class VideoDeleteView(DeleteView):
+    model = Video
+    template_name = 'videos/video_delete.html'
+    success_url = reverse_lazy('video_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class VideoCreateView(CreateView):
+    model = Video
+    template_name = 'videos/video_new.html'
+    fields = ('title', 'video',)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
