8acdd1ba84cc544ec6e7e88c0710ed396c489331
Charles Montemayor <montemayorcharles@outlook.com>
2020-09-17 18:07:26 -0600

    allowed host name changed

--- lets_study_japanese/settings.py
+++ lets_study_japanese/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['monicacabreza.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['lets-study-japanese.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
